6b60a0e274b997958a312b6f3d80ae9335fe0530
mohamed <mohkori50@gmail.com>
2026-01-01 13:59:57 +0100

    تعديل رسالة الترحيب في الصفحة الرئيسية

--- courses_mohamed/settings.py
+++ courses_mohamed/settings.py
@@ -1,7 +1,13 @@
 STATIC_URL = 'static/'
+# أضف هذا السطر لضمان عمل ملفات الـ CSS والـ Matrix على Render
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# تعاريف الميديا - تأكد أنها مكتوبة مرة واحدة فقط وبشكل صحيح
+# تعاريف الميديا - سليمة تماماً
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# إضافة هامة جداً لـ Render لإصلاح خطأ الـ Port
+import os
+PORT = os.environ.get('PORT', '8000')
