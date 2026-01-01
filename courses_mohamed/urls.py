8de3073870d8e3f31b181242c4f184c5b93715a7
mohamed <mohkori50@gmail.com>
2026-01-01 13:30:11 +0100

    تعديل رسالة الترحيب في الصفحة الرئيسية

--- courses_mohamed/urls.py
+++ courses_mohamed/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # هذا السطر هو الذي سيحل مشكلة 'login' فوراً
     path('accounts/', include('django.contrib.auth.urls')), 
-    path('', include('courses.urls')), # تأكد من اسم تطبيقك هنا
+    # قمنا بتغيير 'courses.urls' إلى 'courses_mohamed.urls'
+    path('', include('courses_mohamed.urls')), 
 ]
