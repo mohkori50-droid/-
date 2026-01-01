75ffc0e59305c53f2220eae76c75adb4f8878631
mohamed <mohkori50@gmail.com>
2026-01-01 12:52:28 +0100

    تعديل رسالة الترحيب في الصفحة الرئيسية

--- courses_mohamed/urls.py
+++ courses_mohamed/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from . import views  # استيراد ملف views.py الخاص بك
+from . import views
 
 urlpatterns = [
-    # الرابط الأساسي الذي تظهر فيه الكروت النيون
     path('', views.course_list, name='course_list'),
-
-    # الرابط الجديد الذي سيفعل زر "فتح البيانات" لكل درس
-    path('course/<int:pk>/', views.course_detail, name='course_detail'),
+    
+    # تأكد أن الاسم هنا هو 'course_detail' ليتوافق مع زر الـ HTML
+    # وتأكد أنه يستدعي دالة 'video_detail' الموجودة في الـ views
+    path('course/<int:course_id>/', views.video_detail, name='course_detail'),
 ]
