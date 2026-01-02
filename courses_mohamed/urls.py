525b612c589fec0105cee8e96d0f2ce018c57763
mohamed <mohkori50@gmail.com>
2026-01-02 14:50:10 +0100

    تعديل رسالة الترحيب في الصفحة الرئيسية

--- courses_mohamed/urls.py
+++ courses_mohamed/urls.py
@@ -7,3 +7,8 @@ urlpatterns = [
     path('', views.course_list, name='course_list'),
     path('video/<int:course_id>/', views.video_detail, name='video_detail'), # هذا السطر كان ناقصاً
 ]
+from django.contrib.auth.models import User
+
+# كود مؤقت لإنشاء مستخدم آلياً
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', '12345678')
