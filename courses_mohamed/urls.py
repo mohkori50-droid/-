29ba82df8d8939b04a9b1ea5184504cd042c1a02
mohamed <mohkori50@gmail.com>
2026-01-02 14:26:23 +0100

    تعديل رسالة الترحيب في الصفحة الرئيسية

--- courses_mohamed/urls.py
+++ courses_mohamed/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from . import views  # استيراد من نفس المجلد
+from . import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.course_list, name='course_list'), # تأكد من اسم الدالة في views.py
+    path('', views.course_list, name='course_list'),
+    path('video/<int:course_id>/', views.video_detail, name='video_detail'), # هذا السطر كان ناقصاً
 ]
